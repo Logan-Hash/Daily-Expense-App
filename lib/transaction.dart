efb3c74a25883eeb4ae511fde5388a18538e712b
Pyae Phyo Thant <pyaephyothant1998@gmail.com>
2020-11-09 20:31:54 +0630

    done myanmar date and etc..

---
+++ lib/transaction.dart
@@ -0,0 +1,15 @@
+class Transaction {
+  final String id;
+  final String title;
+  final int amount;
+  final DateTime date;
+  final String currency;
+
+  Transaction({
+    this.id,
+    this.title,
+    this.amount,
+    this.date,
+    this.currency,
+  });
+}
